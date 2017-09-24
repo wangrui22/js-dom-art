3d7bc682d375382e53a66d9193807c3bd86bb94b
chrisworry <wangrui22bd@gmail.com>
2017-09-24 23:10:47 +0800

    optimize

--- ch4-js-img/index.js
+++ ch4-js-img/index.js
@@ -1,15 +1,35 @@
 (function(){
 
-    // function showPic(pic) {
-    //     var source = pic.getAttribute('href');
-    //     var placeHolder = document.getElementById('place-holder');
-    //     placeHolder.setAttribute("src", source);
-    // }
+    function prepareGallery() {
+        if (!document.getElementsByTagName) return false;
+        if (!document.getElementById) return false;
+
+        gallery = document.getElementById('gallery');
+        if (!gallery) return false;
+        var links = document.getElementsByTagName('a');
+        for ( var i=0; i < links.length; i++) {
+          links[i].onclick = function() {
+            return !showPic(this);
+          }
+          //links[i].onkeypress = links[i].onclick;
+        }
+      }
+
+      prepareGallery();
 
 })();
 
 function showPic(pic) {
+    if (!document.getElementById('place-holder')) return false;
     var source = pic.getAttribute('href');
     var placeHolder = document.getElementById('place-holder');
-    placeHolder.setAttribute("src", source);
+    placeHolder.setAttribute('src', source);
+
+    if (document.getElementById('description')) {
+        var text = pic.getAttribute('title');
+        document.getElementById('description').innerHTML = text;
+    }
+    return true;
 }
+
+
