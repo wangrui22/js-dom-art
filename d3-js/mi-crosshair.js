516aa853da1df6f13a2794724ebf244fce29a029
chrisworry <wangrui22bd@gmail.com>
2017-10-15 18:03:03 +0800

    add crosshair

--- d3-js/mi-crosshair.js
+++ d3-js/mi-crosshair.js
@@ -1,6 +1,11 @@
 
-const LINE_WIDTH = 1;
-const LINE_COLOR = 'red'
+const LINE_WIDTH = 2;
+const CROSS_WIDTH = 1;
+const LINE_COLOR = 'red';
+const CROSS_COLOR = 'blue';
+const CROSS_SIZE = 20;
+const CROSS_CTRL_SIZE = 7;
+
 function Crosshair(svg, cx, cy, line0Para, line1Para) {
     this.svg = svg;
     this.cx = cx;
@@ -17,6 +22,8 @@ function Crosshair(svg, cx, cy, line0Para, line1Para) {
     this.crossDown;
     this.crossLeft;
     this.crossRight;
+    this.crossCtrlOverlay;
+    this.crossCtrl;
 
     //TODO
     //ctrl
@@ -24,86 +31,159 @@ function Crosshair(svg, cx, cy, line0Para, line1Para) {
     // this.line00Rotate;
 
     //init
-    var l0 = this.calLine(cx, cy, line0Para);
     this.line00 = d3.select(this.svg)
     .append('line')
-    .attr('x1', l0[0].x)
-    .attr('y1', l0[0].y)
-    .attr('x2', l0[1].x)
-    .attr('y2', l0[1].y)
     .style('stroke-width', LINE_WIDTH)
     .style('stroke', LINE_COLOR)
     .style('stroke-opacity', 1.0)
-    .style('cursor', 'move');
 
-    var l1 = this.calLine(cx, cy, line1Para);
+    this.line01 = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', LINE_WIDTH)
+    .style('stroke', LINE_COLOR)
+    .style('stroke-opacity', 1.0)
+
     this.line10 = d3.select(this.svg)
     .append('line')
-    .attr('x1', l1[0].x)
-    .attr('y1', l1[0].y)
-    .attr('x2', l1[1].x)
-    .attr('y2', l1[1].y)
     .style('stroke-width', LINE_WIDTH)
     .style('stroke', LINE_COLOR)
     .style('stroke-opacity', 1.0)
+
+    this.line11 = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', LINE_WIDTH)
+    .style('stroke', LINE_COLOR)
+    .style('stroke-opacity', 1.0)
+
+    this.crossUp = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', CROSS_WIDTH)
+    .style('stroke', CROSS_COLOR)
+    .style('stroke-opacity', 1.0)
+
+    this.crossDown = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', CROSS_WIDTH)
+    .style('stroke', CROSS_COLOR)
+    .style('stroke-opacity', 1.0)
+
+    this.crossLeft = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', CROSS_WIDTH)
+    .style('stroke', CROSS_COLOR)
+    .style('stroke-opacity', 1.0)
+
+    this.crossRight = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', CROSS_WIDTH)
+    .style('stroke', CROSS_COLOR)
+    .style('stroke-opacity', 1.0)
+
+    this.crossCtrlOverlay = d3.select(this.svg)
+    .append('rect')
+    .style('fill', 'white')
+    .style('opacity', 0)
+
+    this.crossCtrl = d3.select(this.svg)
+    .append('rect')
+    .style('fill', 'white')
+    .style('opacity', 0)
     .style('cursor', 'move');
 
-    //drag line
-    this.line00.call(d3.drag().
+    this.setLine(cx, cy, line0Para, line1Para);
+
+    
+
+    //drag crosshair
+    this.crossCtrl.call(d3.drag().
     on('drag', (function (d) {
         var x = d3.event.x;
         var y = d3.event.y;
+        //calculate new line parameter
+        
         if (this.line0Para.a == 0) {
             this.line0Para.b = 1/y;
-            var l0 = this.calLine(this.cx, this.cy, this.line0Para);
-            this.line00.attr('x1', l0[0].x)
-            .attr('y1', l0[0].y)
-            .attr('x2', l0[1].x)
-            .attr('y2', l0[1].y);
         } else if (line0Para.b == 0) {
             this.line0Para.a = 1/x;
-            var l0 = this.calLine(this.cx, this.cy, this.line0Para);
-            this.line00.attr('x1', l0[0].x)
-            .attr('y1', l0[0].y)
-            .attr('x2', l0[1].x)
-            .attr('y2', l0[1].y);
         } else {
 
         }
 
-        console.log(d3.event.x + ' ' + d3.event.y);
-    }).bind(this))
-    .on('end', (function (d) {
-        console.log(d3.event.x + ' ' + d3.event.y);
-    }).bind(this)));
-
-    this.line10.call(d3.drag().
-    on('drag', (function (d) {
-        var x = d3.event.x;
-        var y = d3.event.y;
         if (this.line1Para.a == 0) {
             this.line1Para.b = 1/y;
-            var l0 = this.calLine(this.cx, this.cy, this.line1Para);
-            this.line10.attr('x1', l0[0].x)
-            .attr('y1', l0[0].y)
-            .attr('x2', l0[1].x)
-            .attr('y2', l0[1].y);
         } else if (line1Para.b == 0) {
             this.line1Para.a = 1/x;
-            var l0 = this.calLine(this.cx, this.cy, this.line1Para);
-            this.line10.attr('x1', l0[0].x)
-            .attr('y1', l0[0].y)
-            .attr('x2', l0[1].x)
-            .attr('y2', l0[1].y);
         } else {
 
         }
 
+        this.setLine(x, y, this.line0Para, this.line1Para);
         console.log(d3.event.x + ' ' + d3.event.y);
     }).bind(this))
     .on('end', (function (d) {
         console.log(d3.event.x + ' ' + d3.event.y);
     }).bind(this)));
+
+
+    //drag line
+    // this.line00.call(d3.drag().
+    // on('drag', (function (d) {
+    //     var x = d3.event.x;
+    //     var y = d3.event.y;
+    //     if (this.line0Para.a == 0) {
+    //         this.line0Para.b = 1/y;
+    //         var l0 = this.calLine(this.cx, this.cy, this.line0Para);
+    //         this.line00.attr('x1', l0[0].x)
+    //         .attr('y1', l0[0].y)
+    //         .attr('x2', l0[1].x)
+    //         .attr('y2', l0[1].y);
+    //     } else if (line0Para.b == 0) {
+    //         this.line0Para.a = 1/x;
+    //         var l0 = this.calLine(this.cx, this.cy, this.line0Para);
+    //         this.line00.attr('x1', l0[0].x)
+    //         .attr('y1', l0[0].y)
+    //         .attr('x2', l0[1].x)
+    //         .attr('y2', l0[1].y);
+    //     } else {
+
+    //     }
+    //     console.log(d3.event.x + ' ' + d3.event.y);
+    // }).bind(this))
+    // .on('end', (function (d) {
+    //     console.log(d3.event.x + ' ' + d3.event.y);
+    // }).bind(this)));
+
+    // this.line10.call(d3.drag().
+    // on('drag', (function (d) {
+    //     var x = d3.event.x;
+    //     var y = d3.event.y;
+    //     if (this.line1Para.a == 0) {
+    //         this.line1Para.b = 1/y;
+    //         var l0 = this.calLine(this.cx, this.cy, this.line1Para);
+    //         this.line10.attr('x1', l0[0].x)
+    //         .attr('y1', l0[0].y)
+    //         .attr('x2', l0[1].x)
+    //         .attr('y2', l0[1].y);
+    //     } else if (line1Para.b == 0) {
+    //         this.line1Para.a = 1/x;
+    //         var l0 = this.calLine(this.cx, this.cy, this.line1Para);
+    //         this.line10.attr('x1', l0[0].x)
+    //         .attr('y1', l0[0].y)
+    //         .attr('x2', l0[1].x)
+    //         .attr('y2', l0[1].y);
+    //     } else {
+
+    //     }
+
+    //     console.log(d3.event.x + ' ' + d3.event.y);
+    // }).bind(this))
+    // .on('end', (function (d) {
+    //     console.log(d3.event.x + ' ' + d3.event.y);
+    // }).bind(this)));
+}
+
+function distance(p0, p1) {
+    return Math.sqrt((p0.x - p1.x)*(p0.x - p1.x) + (p0.y - p1.y)*(p0.y - p1.y));
 }
 
 Crosshair.prototype.calLine = function(cx, cy, para) {
@@ -125,16 +205,87 @@ Crosshair.prototype.calLine = function(cx, cy, para) {
     var x3 = (1 - para.b * y3) / para.a;
     if (x3 >= 0 && x3 <= width - 1) { res.push({x:x3, y:y3});}
     if (res.length != 2)  {
-        return [{x:0, y:0}, {x:0,y:0}];
+        return [{x:0, y:0}, {x:0,y:0}, {x:0, y:0}, {x:0,y:0}];
+    }
+
+    var ab = Math.sqrt(para.a*para.a + para.b*para.b);
+    var dx = para.b/ab;
+    var dy = para.a/ab;
+    var p0 = {x:cx - CROSS_SIZE*dx, y:cy - CROSS_SIZE*dy};
+    var p1 = {x:cx + CROSS_SIZE*dx, y:cy + CROSS_SIZE*dy};
+
+    if (distance(res[0], p0) <= distance(res[0], {x:this.cx, y:this.cy})) {
+        return [res[0], p0, res[1], p1];
     } else {
-        return res;
+        return [res[0], p1, res[1], p0];
     }
 }
-// Crosshair.prototype.setLine = function(cx, cy, line0, line1) {
-//     this.cx = cx;
-//     this.cy = cy;
-//     this.line0 = line0;//{dx: dy:}
-//     this.line1 = line1;//{dx: dy:}
 
+Crosshair.prototype.setLine = function(cx, cy, para0, para1) {
+    this.cx = cx;
+    this.cy = cy;
+    this.line0Para = para0;//{a: b:} ax + by = 1
+    this.line1Para = para1;//{a: b:}
 
-// }
+    var l0 = this.calLine(this.cx, this.cy, this.line0Para);
+    this.line00
+    .attr('x1', l0[0].x)
+    .attr('y1', l0[0].y)
+    .attr('x2', l0[1].x)
+    .attr('y2', l0[1].y);
+
+    this.line01
+    .attr('x1', l0[2].x)
+    .attr('y1', l0[2].y)
+    .attr('x2', l0[3].x)
+    .attr('y2', l0[3].y);
+
+    var l1 = this.calLine(this.cx, this.cy, this.line1Para);
+    this.line10
+    .attr('x1', l1[0].x)
+    .attr('y1', l1[0].y)
+    .attr('x2', l1[1].x)
+    .attr('y2', l1[1].y);
+
+    this.line11
+    .attr('x1', l1[2].x)
+    .attr('y1', l1[2].y)
+    .attr('x2', l1[3].x)
+    .attr('y2', l1[3].y);
+
+    this.crossLeft
+    .attr('x1', this.cx)
+    .attr('y1', this.cy)
+    .attr('x2', this.cx - CROSS_SIZE/2)
+    .attr('y2', this.cy);
+
+    this.crossRight
+    .attr('x1', this.cx)
+    .attr('y1', this.cy)
+    .attr('x2', this.cx + CROSS_SIZE/2)
+    .attr('y2', this.cy);
+
+    this.crossUp
+    .attr('x1', this.cx)
+    .attr('y1', this.cy)
+    .attr('x2', this.cx)
+    .attr('y2', this.cy - CROSS_SIZE/2);
+
+    this.crossDown
+    .attr('x1', this.cx)
+    .attr('y1', this.cy)
+    .attr('x2', this.cx)
+    .attr('y2', this.cy + CROSS_SIZE/2);
+
+    this.crossCtrlOverlay
+    .attr('x', cx - CROSS_SIZE/2)
+    .attr('y', cy - CROSS_SIZE/2)
+    .attr('width', CROSS_SIZE)
+    .attr('height', CROSS_SIZE);
+
+    this.crossCtrl
+    .attr('x', cx - CROSS_CTRL_SIZE/2)
+    .attr('y', cy - CROSS_CTRL_SIZE/2)
+    .attr('width', CROSS_CTRL_SIZE)
+    .attr('height', CROSS_CTRL_SIZE);
+}
