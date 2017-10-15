292f8619675f79b8fd463e462e228532be2b4e1d
chrisworry <wangrui22bd@gmail.com>
2017-10-15 21:52:34 +0800

    add line move

--- d3-js/mi-crosshair.js
+++ d3-js/mi-crosshair.js
@@ -6,7 +6,7 @@ function Crosshair(svg, cx, cy, line0Para, line1Para) {
     this.line0Para = line0Para;//{a: b:} ax + by = 1
     this.line1Para = line1Para;//{a: b:}
 
-    //svg
+    //main
     this.line00;
     this.line01;
     this.line10;
@@ -15,8 +15,17 @@ function Crosshair(svg, cx, cy, line0Para, line1Para) {
     this.crossDown;
     this.crossLeft;
     this.crossRight;
+
+    //crosshair
     this.crossCtrlOverlay;
     this.crossCtrl;
+
+    //move
+    this.line00CtrlMove;
+    this.line01CtrlMove;
+    this.line10CtrlMove;
+    this.line11CtrlMove;
+
 
     //TODO
     //ctrl
@@ -28,54 +37,82 @@ function Crosshair(svg, cx, cy, line0Para, line1Para) {
     .append('line')
     .style('stroke-width', this.lineWidth)
     .style('stroke', this.line0Color)
-    .style('stroke-opacity', 1.0)
+    .style('stroke-opacity', 1.0);
 
     this.line01 = d3.select(this.svg)
     .append('line')
     .style('stroke-width', this.lineWidth)
     .style('stroke', this.line0Color)
-    .style('stroke-opacity', 1.0)
+    .style('stroke-opacity', 1.0);
+
+    this.line00CtrlMove = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', this.lineCtrlWidth)
+    .style('stroke', this.line0Color)
+    .style('stroke-opacity', 0.0)
+    .style('cursor', 'move');
+
+    this.line01CtrlMove = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', this.lineCtrlWidth)
+    .style('stroke', this.line0Color)
+    .style('stroke-opacity', 0.0)
+    .style('cursor', 'move');
 
     this.line10 = d3.select(this.svg)
     .append('line')
     .style('stroke-width', this.lineWidth)
     .style('stroke', this.line1Color)
-    .style('stroke-opacity', 1.0)
+    .style('stroke-opacity', 1.0);
 
     this.line11 = d3.select(this.svg)
     .append('line')
     .style('stroke-width', this.lineWidth)
     .style('stroke', this.line1Color)
-    .style('stroke-opacity', 1.0)
+    .style('stroke-opacity', 1.0);
+
+    this.line10CtrlMove = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', this.lineCtrlWidth)
+    .style('stroke', this.line1Color)
+    .style('stroke-opacity', 0.0)
+    .style('cursor', 'move');
+
+    this.line11CtrlMove = d3.select(this.svg)
+    .append('line')
+    .style('stroke-width', this.lineCtrlWidth)
+    .style('stroke', this.line1Color)
+    .style('stroke-opacity', 0.0)
+    .style('cursor', 'move');
 
     this.crossUp = d3.select(this.svg)
     .append('line')
     .style('stroke-width', this.crossWidth)
     .style('stroke', this.crossColor)
-    .style('stroke-opacity', 1.0)
+    .style('stroke-opacity', 1.0);
 
     this.crossDown = d3.select(this.svg)
     .append('line')
     .style('stroke-width', this.crossWidth)
     .style('stroke', this.crossColor)
-    .style('stroke-opacity', 1.0)
+    .style('stroke-opacity', 1.0);
 
     this.crossLeft = d3.select(this.svg)
     .append('line')
     .style('stroke-width', this.crossWidth)
     .style('stroke', this.crossColor)
-    .style('stroke-opacity', 1.0)
+    .style('stroke-opacity', 1.0);
 
     this.crossRight = d3.select(this.svg)
     .append('line')
     .style('stroke-width', this.crossWidth)
     .style('stroke', this.crossColor)
-    .style('stroke-opacity', 1.0)
+    .style('stroke-opacity', 1.0);
 
     this.crossCtrlOverlay = d3.select(this.svg)
     .append('rect')
     .style('fill', 'white')
-    .style('opacity', 0)
+    .style('opacity', 0);
 
     this.crossCtrl = d3.select(this.svg)
     .append('rect')
@@ -85,7 +122,6 @@ function Crosshair(svg, cx, cy, line0Para, line1Para) {
 
     this.setLine(cx, cy, line0Para, line1Para);
 
-    
 
     //drag crosshair
     this.crossCtrl.call(d3.drag().
@@ -94,57 +130,102 @@ function Crosshair(svg, cx, cy, line0Para, line1Para) {
         var y = d3.event.y;
         //calculate new line parameter
         
-        if (this.line0Para.a == 0) {
-            this.line0Para.b = 1/y;
-        } else if (line0Para.b == 0) {
-            this.line0Para.a = 1/x;
-        } else {
+        // if (this.line0Para.a == 0) {
+        //     this.line0Para.b = 1/y;
+        // } else if (line0Para.b == 0) {
+        //     this.line0Para.a = 1/x;
+        // } else {
 
-        }
+        // }
 
-        if (this.line1Para.a == 0) {
-            this.line1Para.b = 1/y;
-        } else if (line1Para.b == 0) {
-            this.line1Para.a = 1/x;
-        } else {
+        // if (this.line1Para.a == 0) {
+        //     this.line1Para.b = 1/y;
+        // } else if (line1Para.b == 0) {
+        //     this.line1Para.a = 1/x;
+        // } else {
+        // }
 
-        }
+        var para = this.line0Para;
+        var ab = Math.sqrt(para.a*para.a + para.b*para.b);
+        var normx = para.a/ab;
+        var normy = para.b/ab;
+        var dis = dot({x:x-this.cx, y:y-this.cy}, {x:normx, y:normy});
+        var newpara0 = {a:normx/(1/ab+dis), b:normy/(1/ab+dis)};  
 
-        this.setLine(x, y, this.line0Para, this.line1Para);
-        console.log(d3.event.x + ' ' + d3.event.y);
+        para = this.line1Para;
+        ab = Math.sqrt(para.a*para.a + para.b*para.b);
+        normx = para.a/ab;
+        normy = para.b/ab;
+        dis = dot({x:x-this.cx, y:y-this.cy}, {x:normx, y:normy});
+        var newpara1 = {a:normx/(1/ab+dis), b:normy/(1/ab+dis)};  
+
+        this.setLine(x, y, newpara0, newpara1);
+
+        //TOOD message callback
+
+        //console.log(d3.event.x + ' ' + d3.event.y);
     }).bind(this))
     .on('end', (function (d) {
-        console.log(d3.event.x + ' ' + d3.event.y);
+        //console.log(d3.event.x + ' ' + d3.event.y);
     }).bind(this)));
 
-
     //drag line
-    // this.line00.call(d3.drag().
-    // on('drag', (function (d) {
-    //     var x = d3.event.x;
-    //     var y = d3.event.y;
-    //     if (this.line0Para.a == 0) {
-    //         this.line0Para.b = 1/y;
-    //         var l0 = this.calLine(this.cx, this.cy, this.line0Para);
-    //         this.line00.attr('x1', l0[0].x)
-    //         .attr('y1', l0[0].y)
-    //         .attr('x2', l0[1].x)
-    //         .attr('y2', l0[1].y);
-    //     } else if (line0Para.b == 0) {
-    //         this.line0Para.a = 1/x;
-    //         var l0 = this.calLine(this.cx, this.cy, this.line0Para);
-    //         this.line00.attr('x1', l0[0].x)
-    //         .attr('y1', l0[0].y)
-    //         .attr('x2', l0[1].x)
-    //         .attr('y2', l0[1].y);
-    //     } else {
+    var line0Move = (function(event) {
+        var x = d3.event.x;
+        var y = d3.event.y;
+        var para = this.line0Para;
 
-    //     }
-    //     console.log(d3.event.x + ' ' + d3.event.y);
-    // }).bind(this))
-    // .on('end', (function (d) {
-    //     console.log(d3.event.x + ' ' + d3.event.y);
-    // }).bind(this)));
+        var ab = Math.sqrt(para.a*para.a + para.b*para.b);
+        var normx = para.a/ab;
+        var normy = para.b/ab;
+        var dis = dot({x:x-this.cx, y:y-this.cy}, {x:normx, y:normy});
+        var newcx = this.cx + dis*normx;
+        var newcy = this.cy + dis*normy;
+        var newpara = {a:normx/(1/ab+dis), b:normy/(1/ab+dis)};  
+
+        this.setLine(newcx, newcy, newpara, this.line1Para);
+    }).bind(this);
+
+    this.line00CtrlMove.call(d3.drag()
+    .on('drag', line0Move)
+    .on('end', (function (d) {
+        //console.log(d3.event.x + ' ' + d3.event.y);
+    }).bind(this)));
+
+    this.line01CtrlMove.call(d3.drag()
+    .on('drag', line0Move)
+    .on('end', (function (d) {
+        //console.log(d3.event.x + ' ' + d3.event.y);
+    }).bind(this)));
+
+    var line1Move = (function(event) {
+        var x = d3.event.x;
+        var y = d3.event.y;
+        var para = this.line1Para;
+
+        var ab = Math.sqrt(para.a*para.a + para.b*para.b);
+        var normx = para.a/ab;
+        var normy = para.b/ab;
+        var dis = dot({x:x-this.cx, y:y-this.cy}, {x:normx, y:normy});
+        var newcx = this.cx + dis*normx;
+        var newcy = this.cy + dis*normy;
+        var newpara = {a:normx/(1/ab+dis), b:normy/(1/ab+dis)};  
+
+        this.setLine(newcx, newcy, this.line0Para, newpara);
+    }).bind(this);
+
+    
+    this.line10CtrlMove.call(d3.drag()
+    .on('drag', line1Move)
+    .on('end', (function (d) {
+        //console.log(d3.event.x + ' ' + d3.event.y);
+    }).bind(this)));
+
+    this.line11CtrlMove.call(d3.drag()
+    .on('drag', line1Move)
+    .on('end', (function (d) {
+        //console.log(d3.event.x + ' ' + d3.event.y);
+    }).bind(this)));
 
     // this.line10.call(d3.drag().
     // on('drag', (function (d) {
@@ -179,7 +260,12 @@ function distance(p0, p1) {
     return Math.sqrt((p0.x - p1.x)*(p0.x - p1.x) + (p0.y - p1.y)*(p0.y - p1.y));
 }
 
+function dot(a, b) {
+    return a.x*b.x + a.y*b.y;
+}
+
 Crosshair.prototype.lineWidth = 1.5;
+Crosshair.prototype.lineCtrlWidth = 5;
 Crosshair.prototype.line0Color ='red';
 Crosshair.prototype.line1Color = 'yellow';
 Crosshair.prototype.crossColor = 'blue';
@@ -240,6 +326,18 @@ Crosshair.prototype.setLine = function(cx, cy, para0, para1) {
     .attr('x2', l0[3].x)
     .attr('y2', l0[3].y);
 
+    this.line00CtrlMove
+    .attr('x1', l0[0].x)
+    .attr('y1', l0[0].y)
+    .attr('x2', l0[1].x)
+    .attr('y2', l0[1].y);
+
+    this.line01CtrlMove
+    .attr('x1', l0[2].x)
+    .attr('y1', l0[2].y)
+    .attr('x2', l0[3].x)
+    .attr('y2', l0[3].y);
+
     var l1 = this.calLine(this.cx, this.cy, this.line1Para);
     this.line10
     .attr('x1', l1[0].x)
@@ -248,6 +346,18 @@ Crosshair.prototype.setLine = function(cx, cy, para0, para1) {
     .attr('y2', l1[1].y);
 
     this.line11
+    .attr('x1', l1[2].x)
+    .attr('y1', l1[2].y)
+    .attr('x2', l1[3].x)
+    .attr('y2', l1[3].y);
+
+    this.line10CtrlMove
+    .attr('x1', l1[0].x)
+    .attr('y1', l1[0].y)
+    .attr('x2', l1[1].x)
+    .attr('y2', l1[1].y);
+
+    this.line11CtrlMove
     .attr('x1', l1[2].x)
     .attr('y1', l1[2].y)
     .attr('x2', l1[3].x)
