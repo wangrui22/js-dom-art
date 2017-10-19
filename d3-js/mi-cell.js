e1c5f20feef632dfc2b6f7ce4c6ba09b7b97fd52
chrisworry <wangrui22bd@gmail.com>
2017-10-19 21:21:00 +0800

    add mouse touch interaction

--- d3-js/mi-cell.js
+++ d3-js/mi-cell.js
@@ -19,22 +19,101 @@ function Cell(cellName, cellID, canvas, svg) {
         var mouseDown_ = (function(event) {
             this.mouseDown(event);
         }).bind(this);
-        this.svg.addEventListener('mousedown', mouseDown_);
+        var touchDown_ = (function(event) {
+            this.touchDown(event);
+        }).bind(this);
+        this.svg.addEventListener('mousedown', mouseDown_, false);
+        this.svg.addEventListener('touchstart', touchDown_, false);
+
 
         var mouseMove_ = (function(event) {
             this.mouseMove(event);
         }).bind(this);
-        this.svg.addEventListener('mousemove', mouseMove_);
+        var touchMove_ = (function(event) {
+            this.touchMove(event);
+        }).bind(this);
+        this.svg.addEventListener('mousemove', mouseMove_, false);
+        this.svg.addEventListener('touchmove', touchMove_, false);
 
         var mouseUp_ = (function(event) {
             this.mouseUp(event);
         }).bind(this);
-        this.svg.addEventListener('mouseup', mouseUp_);
+        var touchUp_ = (function(event) {
+            this.touchUp(event);
+        }).bind(this);
+        this.svg.addEventListener('mouseup', mouseUp_, false);
+        this.svg.addEventListener('touchend', touchUp_, false);
     }
 
     //crosshair
     this.crosshair = new Crosshair(this.svg, $(this.svg).attr('width')/2, $(this.svg).attr('height')/2,
      {a:2/$(this.svg).attr('width'), b:0}, {a:0, b:2/$(this.svg).attr('height')});
+}
+
+Cell.prototype.touchDown = function(event) {
+    var event = event || window.event;
+    event.preventDefault();
+    var oInp = document.getElementById("inp"); 
+    oInp.innerHTML = "Touch started (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";  
+
+
+    this.mouseStatus = BTN_DOWN;
+    this.mouseBtn = BTN_LEFT;
+
+    var x = event.touches[0].clientX - this.svg.getBoundingClientRect().left;
+    var y = event.touches[0].clientY - this.svg.getBoundingClientRect().top;
+
+    if(this.mouseAction) {
+        this.mouseAction.mouseDown(this.mouseBtn, this.mouseStatus, x, y, this);
+    }
+
+    // if(this.mouseAction == ACTION_ID_ADD_ROI_CIRCLE) {
+    //     this.lastROI = new ROICircle(this.rois.length, this.svg, x, y, 1);
+    // }
+}
+
+Cell.prototype.touchMove = function (event) {
+    var event = event || window.event;
+    event.preventDefault();
+    var oInp = document.getElementById("inp"); 
+    oInp.innerHTML = "Touch move (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";  
+    
+    if (this.mouseStatus != BTN_DOWN) {
+        return;
+    }
+
+    var x = event.touches[0].clientX - this.svg.getBoundingClientRect().left;
+    var y = event.touches[0].clientY - this.svg.getBoundingClientRect().top;
+    // if (this.mouseAction == ACTION_ID_ADD_ROI_CIRCLE) {
+    //     this.lastROI.creating(curX, curY);
+    // }
+
+    if (this.mouseAction) {
+        this.mouseAction.mouseMove(this.mouseBtn, this.mouseStatus, x, y, this);
+    }
+}
+
+Cell.prototype.touchUp = function(event) {
+    var event = event || window.event;
+    event.preventDefault();
+    var oInp = document.getElementById("inp"); 
+    oInp.innerHTML = "Touch end (" + event.changedTouches[0].clientX + "," + event.changedTouches[0].clientY + ")"; 
+
+    this.mouseBtn = BTN_NONE;
+    this.mouseStatus = BTN_UP;
+
+    // send a msg to notify BE we are done with adding an circle
+    // if (this.mouseAction == ACTION_ID_ADD_ROI_CIRCLE) {
+    //     this.rois.push(this.lastROI);
+    //     this.lastROI = null;
+    // }
+
+    var x = event.changedTouches[0].clientX - this.svg.getBoundingClientRect().left;
+    var y = event.changedTouches[0].clientY - this.svg.getBoundingClientRect().top;
+
+    if(this.mouseAction) {
+        this.mouseAction.mouseUp(this.mouseBtn, this.mouseStatus, x, y, this);
+    }
 }
 
 
